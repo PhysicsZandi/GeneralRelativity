749a6da71b64dbbb7f0778547368310a7b389ca9
Matteo <129991977+PhysicsZandi@users.noreply.github.com>
2024-02-08 15:23:38 +0100

    Update

---
+++ source/py/py.py
@@ -0,0 +1,16 @@
+class Nome: 
+    def __init__(self, g):
+        self.g = g
+
+    def set(self):
+        self.g = 0
+
+    def printmet(self):
+        print("This is", g) 
+
+
+S = Nome(met) 
+S.set()
+S.printmet()
+        
+        
